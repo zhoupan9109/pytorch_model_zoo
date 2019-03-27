b48f4f0e19fec58a15d772fef1374a697076f000
ZHOU PAN <zhoupan9109@163.com>
2019-03-27 12:29:20 +0800

    Create VGG16_Training.py

---
+++ model/VGG16/VGG16_Training.py
@@ -0,0 +1,61 @@
+import torch
+import VGG16
+import VGG16_InputData
+from torch.utils.data import DataLoader
+from torch.autograd import Variable
+import time
+
+data_path = ""   # traing dataset path
+ 
+
+def VGG16Net_Training():
+    if torch.cuda.is_available():
+        model = VGG16.VGG16().cuda()
+    else:
+        model = VGG16.VGG16()
+
+    trainset = VGG16_InputData.LoadDataset(data_path=data_path)
+    # print(len(trainset))
+    train_loader = DataLoader(dataset=trainset, batch_size=20, shuffle=True)
+
+    lr = 1e-3
+    loss_func = torch.nn.CrossEntropyLoss().cuda()
+    # optimizer = torch.optim.SGD(list(model.parameters())[:], lr=lr, momentum=0.9)
+    optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+
+    for epoch in range(10000):
+        print('epoch {}'.format(epoch + 1))
+        # training-----------------------------
+        train_loss = 0.
+        train_acc = 0.
+        start_time = time.time()
+        for trainData, trainLabel in train_loader:
+            # trainData, trainLabel = Variable(trainData.cuda()), Variable(trainLabel.cuda())
+            trainData, trainLabel = trainData.cuda(), trainLabel.cuda()
+            out = model(trainData)
+            loss = loss_func(out, trainLabel)
+            train_loss += loss.item()
+            pred = torch.max(out, 1)[1]
+            train_correct = (pred == trainLabel).sum()
+            train_acc += train_correct.item()
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        #  if epoch % 100 == 0:
+        print("speed time : {:.3f}".format((time.time() - start_time)))
+        print('Train Loss: {:.6f}, Acc: {:.6f}'.format(train_loss / (len(
+            trainset)), train_acc / (len(trainset))))
+
+        if (epoch + 1) % 10 == 0:
+            sodir = './model/_iter_{}.pth'.format(epoch + 1)
+            print('[5] Model save {}'.format(sodir))
+            torch.save(model.state_dict(), sodir)
+
+        # adjust
+        if (epoch + 1) % 50 == 0:
+            lr = lr / 10
+            optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+
+
+if __name__ == "__main__":
+    VGG16Net_Training()
